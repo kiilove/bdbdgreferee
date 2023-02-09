67416bffd80fa32ece0b1d13fd71a6bab5ddb767
kiilove <jbkim@jncore.com>
2023-02-09 13:29:05 +0900

    commit on "main"

--- src/pages/QrLogin.jsx
+++ src/pages/QrLogin.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import QrReader from "react-qr-reader";
 
 const QrLogin = () => {
@@ -24,8 +24,9 @@ const QrLogin = () => {
 
   return (
     <div className="flex w-full h-full justify-center items-center">
-      <div className="flex w-full h-full p-10 border-2 border-orange-500 flex-col">
+      <div className="flex w-full h-full p-10 border-2 border-orange-500 flex-col justify-start items-center">
         <button
+          className="w-40 h-10 bg-orange-500 text-white"
           onClick={() => {
             setStartScan(!startScan);
           }}
@@ -33,20 +34,21 @@ const QrLogin = () => {
           {startScan ? "카메라 중지" : "카메라 작동"}
         </button>
         {startScan && (
-          <>
+          <div className="flex w-full h-full flex-col">
             <select onChange={(e) => setSelected(e.target.value)}>
-              <option value={"environment"}>Back Camera</option>
               <option value={"user"}>Front Camera</option>
+              <option value={"environment"}>Back Camera</option>
             </select>
-            <QrReader
-              facingMode={selected}
-              delay={1000}
-              onError={handleError}
-              onScan={handleScan}
-              // chooseDeviceId={()=>selected}
-              style={{ width: "300px" }}
-            />
-          </>
+            <div className="flex w-full h-full p-20 justify-center items-center">
+              <QrReader
+                facingMode={selected}
+                delay={1000}
+                onError={handleError}
+                onScan={handleScan}
+                className="w-3/4 h-3/4"
+              />
+            </div>
+          </div>
         )}
         {loadingScan && <p>Loading</p>}
         {data !== "" && <p>{data}</p>}
