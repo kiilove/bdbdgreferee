081a30d54e3aacc61dbb6ff6924a3de230a68a92
kiilove <jbkim@jncore.com>
2023-02-12 10:22:19 +0900

    commit on "main"

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Lobby from "./pages/Lobby";
+import Scoring from "./pages/Scoring";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/lobby" element={<Lobby />} />
+        <Route path="/scoring" element={<Scoring />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Scoring.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Scoring = () => {
+  return (
+    <div className="flex w-full justify-center items-center h-full ">
+      <div
+        className="flex justify-center mt-10 flex-col gap-y-3 py-3 px-10  w-full rounded-md border border-gray-200 shadow-md"
+        style={{ maxWidth: "1000px" }}
+      >
+        채점
+      </div>
+    </div>
+  );
+};
+
+export default Scoring;
