51aef2ff4cadf53837a4a1b0fae95c7d7969ea54
kiilove <jbkim@jncore.com>
2023-02-13 14:18:51 +0900

    commit on "main"

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,28 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAnalytics } from "firebase/analytics";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "bdbdg-referee.firebaseapp.com",
-  projectId: "bdbdg-referee",
-  storageBucket: "bdbdg-referee.appspot.com",
-  messagingSenderId: "383215282065",
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: "G-YEEGYK625K",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "body-36982.firebaseapp.com",
+  projectId: "body-36982",
+  storageBucket: "body-36982.appspot.com",
+  messagingSenderId: "297237107965",
+  appId: "1:297237107965:web:cfa3d5d0f8882d0abb5a94",
+  measurementId: "G-DF39XRHWMK",
+  storageBucket: "body-36982.appspot.com",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
--- src/pages/LocalLogin.jsx
+++ src/pages/LocalLogin.jsx
@@ -1,7 +1,59 @@
-import React from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Encrypter } from "../components/Encrypto";
+import { handleToast } from "../components/HandleToast";
+import { db } from "../firebase";
 
 const LocalLogin = () => {
+  const [resLoginData, setResLoginData] = useState([]);
+  const [loginInfo, setLoginInfo] = useState({});
+
+  const TeltoEmail = async ({ tel }) => {
+    const encryTel = Encrypter(tel);
+    console.log(encryTel);
+    let tempData = [];
+    const findEmailQ = query(
+      collection(db, "referee"),
+      where("refTel", "==", Encrypter(tel.toString()))
+    );
+
+    const queryResult = await getDocs(findEmailQ);
+    console.log(queryResult);
+
+    queryResult.forEach((doc) => {
+      //tempData.push({ ...doc.data() });
+      console.log(doc.data());
+    });
+
+    //console.log(tempData);
+  };
+
+  const handleInputs = (e) => {
+    e.preventDefault();
+    setLoginInfo(() => ({ ...loginInfo, [e.target.name]: e.target.value }));
+  };
+  const handleLogin = async ({ email, PWD }) => {
+    const auth = getAuth();
+    await signInWithEmailAndPassword(auth, email, PWD)
+      .then((user) => {
+        const userInfo = user;
+        return userInfo;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        handleToast({ type: "error", msg: errorMessage });
+      });
+  };
+
+  useEffect(() => {
+    //console.log(loginInfo);
+  }, [loginInfo]);
+
   return (
     <div className="flex w-full h-full justify-center items-center">
       <div className="flex flex-col w-full h-full p-20 gap-y-5">
@@ -11,22 +63,32 @@ const LocalLogin = () => {
         <div className="flex justify-center">
           <input
             type="text"
+            name="refTel"
+            value={loginInfo.refTel}
             className="w-full h-12 rounded-md focus:ring-0 focus:outline-orange-400 border border-gray-400 px-5 font-light"
             placeholder="전화번호"
+            onChange={(e) => handleInputs(e)}
           />
         </div>
         <div className="flex justify-center">
           <input
             type="password"
+            name="refPassword"
+            value={loginInfo.refPassword}
             className="w-full h-12 rounded-md focus:ring-0 focus:outline-orange-400 border border-gray-400 px-5 font-light"
             placeholder="비밀번호"
+            onChange={(e) => handleInputs(e)}
           />
         </div>
-        <Link to="lobby">
-          <button className="w-full h-12 bg-green-500 rounded-md border-gray-300 border">
-            <span className=" text-base font-medium text-white">로그인</span>
-          </button>
-        </Link>
+
+        <button
+          className="w-full h-12 bg-green-500 rounded-md border-gray-300 border"
+          onClick={() => {
+            TeltoEmail({ tel: loginInfo.refTel });
+          }}
+        >
+          <span className=" text-base font-medium text-white">로그인</span>
+        </button>
       </div>
     </div>
   );
